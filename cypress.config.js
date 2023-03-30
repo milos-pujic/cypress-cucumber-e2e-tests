79ed73347aea2a31b213e9a816ecc89b0121740a
Milos Pujic <m.pujic@levi9.com>
2023-03-30 10:18:19 +0200

    Added Cypress Split

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('cypress');
 const createBundler = require('@bahmutov/cypress-esbuild-preprocessor');
 const preprocessor = require('@badeball/cypress-cucumber-preprocessor');
 const createEsbuildPlugin = require('@badeball/cypress-cucumber-preprocessor/esbuild');
+const cypressSplit = require('cypress-split');
 
 async function setupNodeEvents(on, config) {
   // This is required for the preprocessor to be able to generate JSON reports after each run, and more,
@@ -13,6 +14,7 @@ async function setupNodeEvents(on, config) {
       plugins: [createEsbuildPlugin.default(config)]
     })
   );
+  cypressSplit(on, config);
   config.defaultCommandTimeout = 4000;
   // Make sure to return the config object as it might have been modified by the plugin.
   return config;
